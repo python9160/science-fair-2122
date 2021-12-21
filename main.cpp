52c2ba75b3bf30ddac1efa51bd63adcb9d7a8030
Soumyadeep <78623094+python9160@users.noreply.github.com>
2021-12-20 19:00:43 -0500

    Forgot to reset arr

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 using namespace std::chrono;
 
 int main() {
-    std::cout << "Run 1" << std::endl; // Run 1
-    const int n = 100; // number of elements
+    const int n = 1024; // number of elements
+    std::cout << "Run 1, size: " << n << std::endl; // Run 1
 
     int arr[n]; // array of elements
 
@@ -21,11 +21,19 @@ int main() {
     auto duration = duration_cast<microseconds>(stop - start); // duration
     std::cout << "bubbleSort: " << duration.count() << " microseconds" << std::endl; // print duration
 
+    for (int i = 0; i < n; i++) { // reset array
+        arr[i] = rand() % n;
+    }
+
     start = high_resolution_clock::now();
     insertionSort(arr, n);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "insertionSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % n;
+    }
 
     start = high_resolution_clock::now();
     selectionSort(arr, n);
@@ -33,11 +41,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "selectionSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % n;
+    }
+
     start = high_resolution_clock::now();
     mergeSort(arr, 0, n - 1);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "mergeSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % n;
+    }
 
     start = high_resolution_clock::now();
     gnomeSort(arr, n);
@@ -45,11 +61,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "gnomeSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % n;
+    }
+
     start = high_resolution_clock::now();
     quickSort(arr, 0, n - 1);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "quickSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % n;
+    }
 
     start = high_resolution_clock::now();
     heapSort(arr, n);
@@ -60,13 +84,13 @@ int main() {
     // Run 1
     
     
-    std::cout << "\nRun 2" << std::endl; // Run 2
-    const int n2 = 250; // number of elements (2nd run)
+    const int n2 = 2048; // number of elements (2nd run)
+    std::cout << "\nRun 2, size: " << n2 << std::endl; // Run 2
 
     int arr2[n2]; // array of elements (2nd run)
 
-    for (int i = 0; i < n; i++) {
-        arr2[i] = rand() % n;
+    for (int i = 0; i < n2; i++) {
+        arr2[i] = rand() % n2;
     }
     #pragma region sorting
     start = high_resolution_clock::now();
@@ -75,11 +99,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "bubbleSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n2; i++) {
+        arr2[i] = rand() % n2;
+    }
+
     start = high_resolution_clock::now();
     insertionSort(arr2, n2);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "insertionSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n2; i++) {
+        arr2[i] = rand() % n2;
+    }
 
     start = high_resolution_clock::now();
     selectionSort(arr2, n2);
@@ -87,11 +119,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "selectionSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n2; i++) {
+        arr2[i] = rand() % n2;
+    }
+
     start = high_resolution_clock::now();
     mergeSort(arr2, 0, n2 - 1);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "mergeSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n2; i++) {
+        arr2[i] = rand() % n2;
+    }
 
     start = high_resolution_clock::now();
     gnomeSort(arr2, n2);
@@ -99,11 +139,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "gnomeSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n2; i++) {
+        arr2[i] = rand() % n2;
+    }
+
     start = high_resolution_clock::now();
     quickSort(arr2, 0, n2 - 1);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "quickSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n2; i++) {
+        arr2[i] = rand() % n2;
+    }
 
     start = high_resolution_clock::now();
     heapSort(arr2, n2);
@@ -113,13 +161,13 @@ int main() {
     #pragma endregion
     // Run 2
 
-    std::cout << "\nRun 3" << std::endl; // Run 3
-    const int n3 = 500; // number of elements (3rd run)
+    const int n3 = 4096; // number of elements (3rd run)
+    std::cout << "\nRun 3, size: " << n3 << std::endl; // Run 3
 
     int arr3[n3]; // array of elements (3rd run)
 
-    for (int i = 0; i < n; i++) {
-        arr3[i] = rand() % n;
+    for (int i = 0; i < n3; i++) {
+        arr3[i] = rand() % n3;
     }
     #pragma region sorting
     start = high_resolution_clock::now();
@@ -128,11 +176,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "bubbleSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n3; i++) {
+        arr3[i] = rand() % n3;
+    }
+
     start = high_resolution_clock::now();
     insertionSort(arr3, n3);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "insertionSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n3; i++) {
+        arr3[i] = rand() % n3;
+    }
 
     start = high_resolution_clock::now();
     selectionSort(arr3, n3);
@@ -140,11 +196,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "selectionSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n3; i++) {
+        arr3[i] = rand() % n3;
+    }
+
     start = high_resolution_clock::now();
     mergeSort(arr3, 0, n3 - 1);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "mergeSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n3; i++) {
+        arr3[i] = rand() % n3;
+    }
 
     start = high_resolution_clock::now();
     gnomeSort(arr3, n3);
@@ -152,11 +216,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "gnomeSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n3; i++) {
+        arr3[i] = rand() % n3;
+    }
+
     start = high_resolution_clock::now();
     quickSort(arr3, 0, n3 - 1);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "quickSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n3; i++) {
+        arr3[i] = rand() % n3;
+    }
 
     start = high_resolution_clock::now();
     heapSort(arr3, n3);
@@ -166,13 +238,13 @@ int main() {
     #pragma endregion
     // Run 3
 
-    std::cout << "\nRun 4" << std::endl; // Run 4
-    const int n4 = 750; // number of elements (4th run)
+    const int n4 = 8192; // number of elements (4th run)
+    std::cout << "\nRun 4, size: " << n4 << std::endl; // Run 4
 
     int arr4[n4]; // array of elements (4th run)
 
-    for (int i = 0; i < n; i++) {
-        arr4[i] = rand() % n;
+    for (int i = 0; i < n4; i++) {
+        arr4[i] = rand() % n4;
     }
     #pragma region sorting
     start = high_resolution_clock::now();
@@ -181,11 +253,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "bubbleSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n4; i++) {
+        arr4[i] = rand() % n4;
+    }
+
     start = high_resolution_clock::now();
     insertionSort(arr4, n4);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "insertionSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n4; i++) {
+        arr4[i] = rand() % n4;
+    }
 
     start = high_resolution_clock::now();
     selectionSort(arr4, n4);
@@ -193,11 +273,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "selectionSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n4; i++) {
+        arr4[i] = rand() % n4;
+    }
+
     start = high_resolution_clock::now();
     mergeSort(arr4, 0, n4 - 1);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "mergeSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n4; i++) {
+        arr4[i] = rand() % n4;
+    }
 
     start = high_resolution_clock::now();
     gnomeSort(arr4, n4);
@@ -205,11 +293,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "gnomeSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n4; i++) {
+        arr4[i] = rand() % n4;
+    }
+
     start = high_resolution_clock::now();
     quickSort(arr4, 0, n4 - 1);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "quickSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n4; i++) {
+        arr4[i] = rand() % n4;
+    }
 
     start = high_resolution_clock::now();
     heapSort(arr4, n4);
@@ -219,13 +315,13 @@ int main() {
     #pragma endregion
     // Run 4
 
-    std::cout << "\nRun 5" << std::endl; // Run 5
-    const int n5 = 1000; // number of elements (5th run)
+    const int n5 = 16384; // number of elements (5th run)
+    std::cout << "\nRun 5, size: " << n5 << std::endl; // Run 5
 
     int arr5[n5]; // array of elements (5th run)
 
-    for (int i = 0; i < n; i++) {
-        arr5[i] = rand() % n;
+    for (int i = 0; i < n5; i++) {
+        arr5[i] = rand() % n5;
     }
     #pragma region sorting
     start = high_resolution_clock::now();
@@ -234,11 +330,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "bubbleSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n5; i++) {
+        arr5[i] = rand() % n5;
+    }
+
     start = high_resolution_clock::now();
     insertionSort(arr5, n5);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "insertionSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n5; i++) {
+        arr5[i] = rand() % n5;
+    }
 
     start = high_resolution_clock::now();
     selectionSort(arr5, n5);
@@ -246,11 +350,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "selectionSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n5; i++) {
+        arr5[i] = rand() % n5;
+    }
+
     start = high_resolution_clock::now();
     mergeSort(arr5, 0, n5 - 1);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "mergeSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n5; i++) {
+        arr5[i] = rand() % n5;
+    }
 
     start = high_resolution_clock::now();
     gnomeSort(arr5, n5);
@@ -258,11 +370,19 @@ int main() {
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "gnomeSort: " << duration.count() << " microseconds" << std::endl;
 
+    for (int i = 0; i < n5; i++) {
+        arr5[i] = rand() % n5;
+    }
+
     start = high_resolution_clock::now();
     quickSort(arr5, 0, n5 - 1);
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << "quickSort: " << duration.count() << " microseconds" << std::endl;
+
+    for (int i = 0; i < n5; i++) {
+        arr5[i] = rand() % n5;
+    }
 
     start = high_resolution_clock::now();
     heapSort(arr5, n5);
